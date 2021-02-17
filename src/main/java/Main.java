9ca83068296835ffc7df1a08e4ac2d940316fe51
ThreshMain <martinkos007@gmail.com>
2021-02-17 23:12:40 +0100

    Changed port in Main.main and added needed parameters

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,7 @@ import server.TCPServer;
 public class Main {
     public static void main(String[] args) {
         try {
-            TCPServer server = new TCPServer(78, 5, true);
+            TCPServer server = new TCPServer(666, 1,5,1, true);
 
             Thread shutDownHookThread = new Thread(server.getShutDownHook());
             Runtime.getRuntime().addShutdownHook(shutDownHookThread);
